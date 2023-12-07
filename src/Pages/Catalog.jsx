1e3e149ebf3121c6a888241e415078a14333e18b
MaksymBora <maxboraod@gmail.com>
2023-12-07 12:42:01 +0100

    add btn load more

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -5,7 +5,17 @@ const Catalog = () => {
   return (
     <div className="md:max-w-[1280px] p-4 md:px-4 mx-auto">
       <Filter />
-      <CardAuto />
+      <section className="pb-[100px]">
+        <div className="mb-[100px]">
+          <CardAuto />
+        </div>
+        <button
+          type="button"
+          className="text-base font-medium underline text-lightBlueColor dark:text-white mx-auto block"
+        >
+          Load More
+        </button>
+      </section>
     </div>
   );
   f;
