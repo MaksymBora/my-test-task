a97f9bee6bdc9292e60fc381f57ec942b432b799
MaksymBora <maxboraod@gmail.com>
2023-12-07 12:43:03 +0100

    add comment

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -6,6 +6,7 @@ const Catalog = () => {
     <div className="md:max-w-[1280px] p-4 md:px-4 mx-auto">
       <Filter />
       <section className="pb-[100px]">
+        {/* Make <ul> on render */}
         <div className="mb-[100px]">
           <CardAuto />
         </div>
@@ -13,7 +14,7 @@ const Catalog = () => {
           type="button"
           className="text-base font-medium underline text-lightBlueColor dark:text-white mx-auto block"
         >
-          Load More
+          Load more
         </button>
       </section>
     </div>
