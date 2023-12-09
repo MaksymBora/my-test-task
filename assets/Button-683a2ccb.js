c9bcfbe752173dcebf93bc9679801c4765ddcf62
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-12-09 12:08:08 +0000

    Deploying to gh-pages from @ MaksymBora/my-test-task@09b36bfc7cfe463e242ad7fd519380b33977f754 🚀

---
+++ assets/Button-683a2ccb.js
@@ -0,0 +1,4 @@
+import{j as r}from"./index-7d2aaf48.js";const x=({width:e,title:o,onClick:n,type:t})=>r.jsx("button",{type:t||"button",onClick:n,className:`${e&&e} min-w-[150px] m-0 py-[14px] px-[44px] border-none text-white bg-gradient-to-r text-[14px] leading-[20px] self-center from-blue-500
+	   via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 
+	   focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium 
+	   rounded-lg text-sm px-auto  text-center`,children:o});export{x as B};
