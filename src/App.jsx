84960c18441ccb5f9cd8823c35b68294d3317130
MaksymBora <maxboraod@gmail.com>
2023-12-10 01:45:53 +0100

    fix redirect

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="favorites" element={<Favorites />} />
+          <Route path="/catalog/*" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
