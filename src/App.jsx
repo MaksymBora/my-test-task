86abdf12bd95c6b79ce3eaba761604b6e817052d
MaksymBora <maxboraod@gmail.com>
2023-12-10 02:31:41 +0100

    remove commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-
-// import { redirect } from 'react-router-dom';
 import { Layout } from './components/Global/Layout';
 import { Home, Catalog, Favorites } from './components/Routing/routesImport';
-// import { Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -13,8 +10,6 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="catalog" element={<Catalog />} />
           <Route path="favorites" element={<Favorites />} />
-          {/* <Route path="*" element={<Home />} /> */}
-          {/* <Route exact path="*" element={<Navigate to="/" />} /> */}
           <Route path="/*" element={<Navigate raplce to="/" />} />
         </Route>
       </Routes>
