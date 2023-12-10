d11045907974f3588e0181da28c9f81dbcc4ab90
MaksymBora <maxboraod@gmail.com>
2023-12-10 01:53:53 +0100

    update redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Global/Layout';
 import { Home, Catalog, Favorites } from './components/Routing/routesImport';
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -9,9 +9,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/catalog/*" element={<Catalog />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </>
